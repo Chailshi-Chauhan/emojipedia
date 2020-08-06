c4fdb1fa77a03c162222735360b68cb2f21e22e3
Chailshi chauhan <chailshichauhan2000@gmail.com>
2020-08-06 17:14:32 +0530

    Updates

---
+++ precache-manifest.4b2a1226803730f0cc6679f4e79dd29e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f9c3de418399569204c9af82ce7be97",
+    "url": "/emojipedia/index.html"
+  },
+  {
+    "revision": "3cd517c8a4f56e0f1387",
+    "url": "/emojipedia/static/js/2.5c980291.chunk.js"
+  },
+  {
+    "revision": "3b969582fb52851c57f5",
+    "url": "/emojipedia/static/js/main.bf4cd427.chunk.js"
+  },
+  {
+    "revision": "1d28d8731736ae6815ca",
+    "url": "/emojipedia/static/js/runtime-main.990ddc9f.js"
+  }
+]);
